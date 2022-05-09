ae5d6a89e353f04defd1a02215f65c943510735c
Mane Manisha Madhukar <99583995+Manu-M01@users.noreply.github.com>
2022-05-09 12:11:42 +0530

    Add files via upload

---
+++ rest.js
@@ -0,0 +1,59 @@
+
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile("details.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "id": 4
+   }
+}
+
+app.post('/addUser', function (req, res) {
+   // First read existing users.
+   fs.readFile("details.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      data["user4"] = user["user4"];
+      console.log("Data added successfully",data );
+      res.end( JSON.stringify(data));
+   });
+})
+
+
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile("details.json", 'utf8', function (err, data) {
+      var users = JSON.parse( data );
+      var user = users["user" + req.params.id] 
+      console.log( user );
+      res.end( JSON.stringify(user));
+   });
+})
+
+var id = 2;
+
+app.delete('/deleteUser', function (req, res) {
+   // First read existing users.
+   fs.readFile("details.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      delete data["user" + 2];
+       
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+
+var server = app.listen(8081, function () {
+    console.log("Server is listening ::")
+})
+
